939dc3c2b54cc09bce659bb6ab2da4d73f6eb5de
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-28 14:08:22 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,19 +7,34 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  # Build an array that contains (or, "nests") the constants into a single
-  # Array. Ruby constants are denoted by ALL_CAPS
+  array = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
+  ]
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
+  aoh = []
+  aoh << DON_G
+  aoh << JOELLE_VD
+  aoh << PAT_M
+  aoh << KATE_G
+  aoh << BRUCE_G
+  aoh
+  # # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
+    aoh[row][key] = new_value
+    aoh
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
